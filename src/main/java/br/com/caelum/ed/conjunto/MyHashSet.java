9d01b9b73be66ff8bcb1073cc9d71d0e3972ebc8
Felipe Santaniello <felipe.santaniello@viavarejo.com.br>
2023-06-15 08:52:48 -0300

    Alterado

--- src/main/java/br/com/caelum/ed/conjunto/MyHashSet.java
+++ src/main/java/br/com/caelum/ed/conjunto/MyHashSet.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class MyHashSet {
+public class MyHashSet<T>{
 
-    private List<List<String>> tabelaEspalhamento = new ArrayList<>();
-    private int tamanho;
+    private List<List<T>> tabelaEspalhamento = new ArrayList<>();
+    private int tamanho = 0;
 
     public MyHashSet() {
         for(int indice = 0; indice < 26;indice++){
@@ -15,27 +15,47 @@ public class MyHashSet {
         }
     }
 
-    private int calcularIndiceEspalhamento(String palavra){
-        return palavra.toLowerCase().charAt(0) % 26;
+    /**
+     * Método antigo que calculava o indice de espalhamento
+     * */
+//    private int calcularIndiceEspalhamento(String palavra){
+//        return palavra.toLowerCase().charAt(0) % 26;
+//    }
+
+    private int calcularIndiceEspalhamento(T objeto) {
+        int codigoDeEspalhamento = objeto.hashCode();
+        codigoDeEspalhamento = Math.abs(codigoDeEspalhamento);
+        return codigoDeEspalhamento % tabelaEspalhamento.size();
     }
 
-    public void adiciona(String palavra){
-        if(contem(palavra))
-            return;
+//    private int calculaCodigoDeEspalhamento(String palavra) {
+//        int codigo = 1;
+//        for (int i = 0; i < palavra.length(); i++) {
+//            codigo = 31 * codigo + palavra.charAt(i);
+//        }
+//        return codigo;
+//    }
 
-        int indiceEspalhamento = calcularIndiceEspalhamento(palavra);
-        tabelaEspalhamento.get(indiceEspalhamento).add(palavra);
-        tamanho++;
+    public void adiciona(T objeto){
+        if(!contem(objeto)) {
+            this.verificaCarga();
+            int indiceEspalhamento = calcularIndiceEspalhamento(objeto);
+            tabelaEspalhamento.get(indiceEspalhamento).add(objeto);
+            this.tamanho++;
+        }
     }
 
-    public void remove(String palavra){
-        int indiceEspalhamento = calcularIndiceEspalhamento(palavra);
-        tabelaEspalhamento.get(indiceEspalhamento).remove(palavra);
-        tamanho--;
+    public void remove(T objeto){
+        if(contem(objeto)){
+            int indiceEspalhamento = calcularIndiceEspalhamento(objeto);
+            tabelaEspalhamento.get(indiceEspalhamento).remove(objeto);
+            this.tamanho--;
+            this.verificaCarga();
+        }
     }
 
-    public List<String> pegaTodas(){
-        List<String> todasPalavras = new ArrayList<>();
+    public List<T> pegaTodas(){
+        List<T> todasPalavras = new ArrayList<>();
         tabelaEspalhamento.forEach(te->todasPalavras.addAll(te));
         return todasPalavras;
     }
@@ -44,8 +64,51 @@ public class MyHashSet {
         return tamanho;
     }
 
-    public boolean contem(String palavra){
-        int indiceEspalhamento = calcularIndiceEspalhamento(palavra);
-        return tabelaEspalhamento.get(indiceEspalhamento).contains(palavra);
+    public boolean contem(T objeto){
+        int indiceEspalhamento = calcularIndiceEspalhamento(objeto);
+        return tabelaEspalhamento.get(indiceEspalhamento).contains(objeto);
     }
+
+    public void imprimeTabelaEspalhamento(){
+        for (List<T> lista : this.tabelaEspalhamento) {
+            System.out.print("[");
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.print("*");
+            }
+            System.out.println("]");
+        }
+    }
+
+    private void verificaCarga() {
+        int capacidade = this.tabelaEspalhamento.size();
+        // a carga da Tabela é a razão entre o número de elementos e o número de Listas.
+        double carga = (double) this.tamanho / capacidade;
+        /**
+         * Se esta carga ultrapassar o valor 0.75 significa que há uma alta chance de ocorrer colisões. Neste caso, aumentaremos o tamanho da Tabela.
+         * Se a carga ficar menor do que o valor 0.25 significa que a Tabela tem muito espaço vazio. Neste caso, diminuiremos o tamanho dela.
+         *
+         */
+        if (carga > 0.75) {
+            this.redimensionaTabela(capacidade * 2);
+        } else if (carga < 0.25) {
+            /**
+             Se a carga for menor que 0.25 a novaCapacidade será a metade da antiga mas para evitar
+             ficar com uma capacidade muito pequena denimos que o mínimo é 10.
+             **/
+            this.redimensionaTabela(Math.max(capacidade / 2, 10));
+        }
+    }
+
+    private void redimensionaTabela(int novaCapacidade){
+        List<T> palavras = this.pegaTodas();
+        this.tabelaEspalhamento.clear();
+        this.tamanho = 0;
+        for (int i = 0; i < novaCapacidade; i++) {
+            this.tabelaEspalhamento.add(new LinkedList<T>());
+        }
+        for (T objeto : palavras) {
+            this.adiciona(objeto);
+        }
+    }
+
 }
--- src/test/java/br/com/caelum/ed/conjunto/MyHashSetTest.java
+++ src/test/java/br/com/caelum/ed/conjunto/MyHashSetTest.java
@@ -1,5 +1,91 @@
 package br.com.caelum.ed.conjunto;
 
-public class MyHashSetTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
+public class MyHashSetTest {
+    @Test
+    void deve_listar_todas_as_palavras(){
+        MyHashSet<String> conjunto = new MyHashSet();
+        conjunto.adiciona("Rafael");
+        conjunto.adiciona("Ana");
+        conjunto.adiciona("Paulo");
+
+        Assertions.assertEquals("[Ana, Paulo, Rafael]",conjunto.pegaTodas().toString());
+        Assertions.assertEquals(3,conjunto.pegaTodas().size());
+    }
+
+    @Test
+    void deve_remover_palavras(){
+        MyHashSet<String> conjunto = new MyHashSet();
+        conjunto.adiciona("Rafael");
+        conjunto.adiciona("Ana");
+        conjunto.adiciona("Paulo");
+
+        Assertions.assertEquals("[Ana, Paulo, Rafael]",conjunto.pegaTodas().toString());
+
+        conjunto.remove("Rafael");
+        Assertions.assertEquals("[Ana, Paulo]",conjunto.pegaTodas().toString());
+    }
+
+    @Test
+    void deve_testar_se_contem_um_elemento(){
+        MyHashSet<String> conjunto = new MyHashSet();
+        conjunto.adiciona("Rafael");
+        conjunto.adiciona("Ana");
+        conjunto.adiciona("Paulo");
+
+        Assertions.assertTrue(conjunto.contem("Paulo"));
+    }
+
+    @Test
+    void deve_testar_tamanho(){
+        MyHashSet<String> conjunto = new MyHashSet();
+        conjunto.adiciona("Rafael");
+        conjunto.adiciona("Ana");
+        conjunto.adiciona("Paulo");
+
+        System.out.println(conjunto.pegaTodas().toString());
+        Assertions.assertEquals(3, conjunto.tamanho());
+    }
+
+    @Test
+    void deve_imprimir_tabela(){
+        MyHashSet<String> conjunto = new MyHashSet();
+        for (int i = 0; i < 20; i++) {
+            conjunto.adiciona("" + i);
+        }
+        conjunto.imprimeTabelaEspalhamento();
+    }
+
+//    @Test
+//    void deve_testar_tempo_execucao_com_redimensionamento_da_tabela(){
+//        // Teste Com Redimensionamento
+//        long inicioComRedimensionamento = System.currentTimeMillis();
+//        MyHashSet conjuntoComRedimensionamento = new MyHashSet();
+//        for (int i = 0; i < 50000; i++) {
+//            conjuntoComRedimensionamento.adicionaComRedimensionamentoDaTabelaDeEspalhamento("palavra" + i);
+//        }
+//        for (int i = 0; i < 50000; i++) {
+//            conjuntoComRedimensionamento.contem("palavra" + i);
+//        }
+//        long fimComRedimensionamento = System.currentTimeMillis();
+//        double tempoComRedimensionamento = (fimComRedimensionamento - inicioComRedimensionamento) / 1000.0;
+//
+//        // Teste Sem Redimensionamento
+//        long inicioSemRedimensionamento = System.currentTimeMillis();
+//        MyHashSet conjuntoSemRedimensionamento = new MyHashSet();
+//        for (int i = 0; i < 50000; i++) {
+//            conjuntoSemRedimensionamento.adiciona("palavra" + i);
+//        }
+//        for (int i = 0; i < 50000; i++) {
+//            conjuntoSemRedimensionamento.contem("palavra" + i);
+//        }
+//        long fimSemRedimensionamento = System.currentTimeMillis();
+//        double tempoSemRedimensionamento = (fimSemRedimensionamento - inicioSemRedimensionamento) / 1000.0;
+//
+//        // Abaixo verificamos que o tempo sem redimensionamento é mais que o dobro do tempo com redimensionamento
+//        Assertions.assertTrue(tempoSemRedimensionamento > tempoComRedimensionamento * 2);
+//    }
 }
